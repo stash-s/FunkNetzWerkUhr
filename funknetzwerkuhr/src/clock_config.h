4bad77bb3b1ecc398928ab7f1cfd85f845616ac0
stash-s <staszek.salik@gmail.com>
2018-08-31 23:56:04 +0200

    Completely broken now

---
+++ funknetzwerkuhr/src/clock_config.h
@@ -0,0 +1,14 @@
+#ifndef __CLOCK_CONFIG_H__
+#define __CLOCK_CONFIG_H__
+
+const int BLINK_LED = 2;
+const int SHDN_PIN = 4;
+const int LATCH_PIN = 15;
+
+#define PWM_MAX 48
+//#define PWM_INIT (1)
+#define PWM_INIT (32)
+
+//#define USE_HW_CS
+
+#endif // __CLOCK_CONFIG_H__
